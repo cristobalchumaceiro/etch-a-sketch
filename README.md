cc13f7d1346716765340d0cb5b3111d3938387bb
Cristobal Chumaceiro Rangel <176019782+cristobalchumaceiro@users.noreply.github.com>
2025-07-26 10:55:13 +0100

    Implement new drawing modes
    Created functions for new drawing modes "colourise" and "opacity" as
    well as a dispatcher function for checking user input and calling the
    corresponding drawing function. Some changes in index.html with basic
    radio buttons. README.md updated with final project overview.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # etch-a-sketch
-Submission for The Odin Project Etch-a-Sketch project from the Foundations Course
+
+## Introduction
+As part of the Foundations course on [The Odin Project](https://www.theodinproject.com/about), I created this Etch-a-Sketch game using JavaScript and HTML
+
+## Skills
+This is one of the later projects of the foundations course, designed to bring together skills in JS, CSS, and HTML. My DOM manipulation, event handling, and logic design skills were strengthened, separating behaviour using dispatcher functions and dynamically switching drawing modes.
+
+## Conclusion
+A fun project, with some more refinement needed in naming functions and variables, as well as a cleaned-up GUI. 
--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-sketchArea = document.querySelector("#sketchArea")
+const sketchArea = document.querySelector("#sketchArea")
+const modeSelection = document.querySelectorAll("input[type=radio]")
 
 //function to create grid of divs, nested loops with initial starting point of 16 x 16
 
 createPad(16)
 
 function createPad(x) {
-    for (i = 0; i < x; i++) {
-        row = document.createElement("div")
+    for (let i = 0; i < x; i++) {
+        let row = document.createElement("div")
         row.id = "row"
         sketchArea.appendChild(row)
         for (y = 0; y < x; y++) {
-            pixel = document.createElement("div")
+            let pixel = document.createElement("div")
             pixel.id = "pixel"
-            pixel.addEventListener("mouseover", (e) => {
-                e.target.style.backgroundColor = "black"
-            })
+            pixel.addEventListener("mouseover", paintSquares)
             row.appendChild(pixel)
         }
     }
@@ -22,7 +21,7 @@ function createPad(x) {
 
 // button to clear grid and initialise new one based on user input
 
-btn = document.querySelector("#newPad")
+const btn = document.querySelector("#newPad")
 btn.addEventListener("click", newPad)
 
 function newPad() {
@@ -35,4 +34,42 @@ function newPad() {
         }
         x = prompt("Please input a whole number between 1 and 100")
     }
+}
+
+// parent function checks for current mode selection and runs the corresponding function
+
+function paintSquares(e) {
+    for (let i = 0; i < modeSelection.length; i++) {
+        if (modeSelection[i].checked) {
+            let mode = modeSelection[i].value
+            if (mode === "colourise") {
+                colouriseMode(e)
+            }
+            else if (mode === "opacity"){
+                opacityMode(e)
+            }
+            else normalMode(e)
+        }
+    } 
+}
+
+// functions for each mode option for etch-a-sketch
+
+function normalMode(e) {
+    e.target.style.opacity = ""
+    e.target.style.backgroundColor = "black"
+}
+
+function colouriseMode(e) {
+    e.target.style.opacity = ""
+    e.target.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16)
+}
+
+function opacityMode(e) {
+    let currentOpacity = parseFloat(e.target.style.opacity) || 0
+    if (currentOpacity < 1) {
+        currentOpacity += 0.1
+        e.target.style.opacity = currentOpacity
+    }
+    e.target.style.backgroundColor = "black"
 }
